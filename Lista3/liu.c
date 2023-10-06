ae1918332cfffcef9c847c18dbda967ec798499f
Lucas Mateus <lucasmateustds@gmail.com>
2023-10-05 21:48:55 -0300

    :bug: fix: Lista 3 agora 100%

--- Lista3/liu.c
+++ Lista3/liu.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
+// Resumo do Liu Kang:
+
+// Vc receberá dois valores, o T e o G;
+// T é a linha do tempo que vc está (é o número que vc vai usar nas contas);
+// G é quantas vezes vc precisará repetir o código (quantas vezes o seu loop vai repetir);
+// Dado um G, vc precisa tirar a raiz quadrada desse número. Para isso, inclua a biblioteca <math.h> no início.
+// Depois, atribua a função "sqrt(T)" a uma variável. Ex: variavel = sqrt(T);
+// A "variavel" vai receber o valor da raiz quadrada do numero T.
+// Se esse número for par, vc subtrai um e eleva ao quadrado (multiplica por ele mesmo). Esse será o seu novo T.
+// Se esse número for ímpar, vc dobra o número, subtrai um e eleva ao quadrado. Esse número será o seu novo T.
+// Depois de obter seu novo T, vc printa esse valor, e o loop executará novamente com o novo T.
+
 int main() {
-    unsigned long long Tempo;
+    long long Tempo, TempoParsed = 0;
     int Golpes;
 
-    scanf("%llu %d", &Tempo, &Golpes);
+    scanf("%lld %d", &Tempo, &Golpes);
 
     for (int i = 0; i < Golpes; i++) {
-        if (Tempo % 2 == 0) {
-            Tempo = (unsigned long long)pow(sqrt(Tempo) / 2, 2);
+        TempoParsed = sqrt(Tempo);
+        if (TempoParsed % 2 == 0) {
+            Tempo = (TempoParsed - 1) * (TempoParsed - 1);
         } else {
-            Tempo = (unsigned long long)pow(sqrt(Tempo), 2);
+            Tempo = (TempoParsed * 2 - 1) * (TempoParsed * 2 - 1);
         }
-        printf("%llu\n", Tempo);
+        printf("%lld\n", Tempo);
     }
 
     return 0;
